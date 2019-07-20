cf8af6d73c235ce3ba77612b23aea6e7a920be95
M Umair <cyberumiar751@gmail.com>
2019-07-20 11:48:19 +0500

    uploading files

---
+++ src/components/AgeDetection/AgeDetection.js
@@ -0,0 +1,13 @@
+import React from 'react' ;
+
+
+const AgeDetection = ({AgeDetect}) => {
+    return(
+
+     <div>
+         {AgeDetect}
+     </div>
+       
+    );
+}
+export default AgeDetection;
